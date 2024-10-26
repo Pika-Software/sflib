f8acde0cf1aa72d153ed9a06cf07632f380f394b
Ykropcheg <123643183+Ykropcheg@users.noreply.github.com>
2024-10-26 14:44:09 +0300

    BassLoadURL func
    Added ability to play sounds using bass.loadURL

--- sflib.lua
+++ sflib.lua
@@ -6,3 +6,27 @@ local function gitHubLink( gitRep, fileName )
     print( modifiedLink .. fileName )
     --return modifiedLink .. fileName
 end
+
+local sounds = {}
+function bassUrlPlay( url, entity )
+    local sound = sounds[url]        
+    if sound and sound:isValid() then            
+        sound:stop()
+    end
+    bass.loadURL(url, "3d noplay noblock", function(new, _, errtxt)            
+        if not (new and new:isValid()) then
+            print(err)                
+            return
+        end
+        sound = new            
+        new:setFade(20, 400)
+        new:setVolume(2)            
+        new:play()
+        sounds[url] = sound
+        hook.add("think", "snd", function()                
+            if new:isValid() and entity:isValid() then
+                new:setPos(entity:localToWorld(entity:obbCenter()))                
+            end
+        end)        
+    end)
+end
